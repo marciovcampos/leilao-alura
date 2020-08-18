56b5fc9cfaa540c741881834d3de909dbcb1c7c2
Márcio Vinícius <marcio.campos@outlook.com>
2020-08-18 19:39:21 -0300

    add test DeveEncontrarOsTresMaioresLances

--- LeilaoTests/AvaliadorTests.swift
+++ LeilaoTests/AvaliadorTests.swift
@@ -62,5 +62,27 @@ class AvaliadorTests: XCTestCase {
         XCTAssertEqual(1000.0, leiloeiro.maiorLance())
     }
     
+    func testDeveEncontrarOsTresMaioresLances() {
+        let joao = Usuario(nome: "Joao")
+        let maria = Usuario(nome: "Maria")
+        
+        let leilao = Leilao(descricao: "Playstation 4")
+        leilao.propoe(lance: Lance(joao, 300.0))
+        leilao.propoe(lance: Lance(maria, 400.0))
+        leilao.propoe(lance: Lance(joao, 500.0))
+        leilao.propoe(lance: Lance(maria, 600.0))
+        
+        let leiloeiro = Avaliador()
+        leiloeiro.avalia(leilao: leilao)
+        
+        let listaLances = leiloeiro.tresMaiores()
+        
+        XCTAssertEqual(3, listaLances.count)
+        XCTAssertEqual(600.0, listaLances[0].valor)
+        XCTAssertEqual(500.0, listaLances[1].valor)
+        XCTAssertEqual(400.0, listaLances[2].valor)
+        
+    }
+    
 
 }
