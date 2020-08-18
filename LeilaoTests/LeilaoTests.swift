7d6dddf1c5781a0c73d9ec4f4b4613506d03b5b8
Márcio Vinícius <marcio.campos@outlook.com>
2020-08-18 20:07:54 -0300

    Leilao - add testDeveReceberVariosLances

--- LeilaoTests/LeilaoTests.swift
+++ LeilaoTests/LeilaoTests.swift
@@ -31,6 +31,23 @@ class LeilaoTests: XCTestCase {
         
     }
     
+     func testDeveReceberVariosLances() {
+        
+       let leilao = Leilao(descricao: "Macbook Pro 15")
+       XCTAssertEqual(0, leilao.lances?.count)
+       
+       let steveJobs = Usuario(nome: "Steve Jobs")
+       leilao.propoe(lance: Lance(steveJobs, 2000.0))
+        
+        let billGates = Usuario(nome: "Bill Gates")
+        leilao.propoe(lance: Lance(billGates, 1700.0))
+        
+        XCTAssertEqual(2, leilao.lances?.count)
+               
+        XCTAssertEqual(2000.0, leilao.lances?.first?.valor)
+        XCTAssertEqual(1700.0, leilao.lances?[1].valor)
+           
+    }
     
     
 }
