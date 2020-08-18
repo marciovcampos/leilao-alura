95d357bd23f3cfd45b5c589dd7bb5a1b1a89b574
Márcio Vinícius <marcio.campos@outlook.com>
2020-08-18 19:10:27 -0300

    add test DeveEntenderLeilaoComApenasUmlance

--- LeilaoTests/AvaliadorTests.swift
+++ LeilaoTests/AvaliadorTests.swift
@@ -42,5 +42,25 @@ class AvaliadorTests: XCTestCase {
         XCTAssertEqual(400.0, leiloeiro.maiorLance())
         
     }
+    
+    
+    func testDeveEntenderLeilaoComApenasUmLance(){
+        // Cenario
+             
+         let joao = Usuario(nome: "Joao")
+         let leilao = Leilao(descricao: "Playstation 4")
+        leilao.propoe(lance: Lance(joao, 1000))
+         
+         // Acao
+         
+         let leiloeiro = Avaliador()
+         leiloeiro.avalia(leilao: leilao)
+         
+         // Validacao
+             
+        XCTAssertEqual(1000.0, leiloeiro.menorLance())
+        XCTAssertEqual(1000.0, leiloeiro.maiorLance())
+    }
+    
 
 }
