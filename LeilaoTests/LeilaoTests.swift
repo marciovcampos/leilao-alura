62426b98269ceee54a0465fe4b264662facec1eb
Márcio Vinícius <marcio.campos@outlook.com>
2020-08-18 20:04:07 -0300

    Leilao - add testDeveReceberUmLance

--- LeilaoTests/LeilaoTests.swift
+++ LeilaoTests/LeilaoTests.swift
@@ -13,24 +13,24 @@ class LeilaoTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDeveReceberUmLance() {
+        let leilao = Leilao(descricao: "Macbook Pro 15")
+        XCTAssertEqual(0, leilao.lances?.count)
+        
+        let steveJobs = Usuario(nome: "Steve Jobs")
+        leilao.propoe(lance: Lance(steveJobs, 2000.0))
+        
+        XCTAssertEqual(1, leilao.lances?.count)
+        XCTAssertEqual(2000.0, leilao.lances?.first?.valor)
+        
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
     
 }
