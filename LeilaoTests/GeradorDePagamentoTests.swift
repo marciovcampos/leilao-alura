a311f9c4e2233f014fd5844a63009562978c06ce
Márcio Vinícius <marcio.campos@outlook.com>
2020-08-20 16:58:05 -0300

    GeradorDePagamento - add func proximoDiaUtil

--- LeilaoTests/GeradorDePagamentoTests.swift
+++ LeilaoTests/GeradorDePagamentoTests.swift
@@ -11,8 +11,15 @@ import XCTest
 import Cuckoo
 
 class GeradorDePagamentoTests: XCTestCase {
+    
+    var daoFalso:MockLeilaoDao!
+    var avaliador:Avaliador!
+    var pagamentos:MockRepositorioDePagamento!
 
     override func setUp() {
+        daoFalso = MockLeilaoDao().withEnabledSuperclassSpy()
+        avaliador = Avaliador()
+        pagamentos = MockRepositorioDePagamento().withEnabledSuperclassSpy()
     }
 
     override func tearDown() {
@@ -24,17 +31,11 @@ class GeradorDePagamentoTests: XCTestCase {
             .lance(Usuario(nome: "Maria"), 2500.0)
             .constroi()
         
-        let daoFalso = MockLeilaoDao().withEnabledSuperclassSpy()
-        
         stub(daoFalso) { (daoFalso) in
             when(daoFalso.encerrados()).thenReturn([playstation])
         }
-        
-        let avaliadorFalso = Avaliador()
-                
-        let pagamentos = MockRepositorioDePagamento().withEnabledSuperclassSpy()
-        
-        let geradorDePagamento = GeradorDePagamento(daoFalso, avaliadorFalso, pagamentos)
+           
+        let geradorDePagamento = GeradorDePagamento(daoFalso, avaliador, pagamentos)
         geradorDePagamento.gera()
         
         let capturadorDeArgumento = ArgumentCaptor<Pagamento>()
@@ -45,6 +46,41 @@ class GeradorDePagamentoTests: XCTestCase {
         
         XCTAssertEqual(2500.0, pagamentoGerado?.getValor())
     }
+    
+    func testDeveEmpurrarParaProximoDiaUtil(){
+           
+           let iPhone = CriadorDeLeilao().para(descricao: "iPhone")
+               .lance(Usuario(nome: "João"), 2000.0)
+               .lance(Usuario(nome: "Maria"), 2500.0)
+               .constroi()
+        
+        stub(daoFalso){ (daoFalso) in
+            when(daoFalso.encerrados()).thenReturn([iPhone])
+        }
+        
+        let formatador = DateFormatter()
+        formatador.dateFormat = "dd/MM/yyyy"
+        
+        guard let dataAntiga = formatador.date(from: "16/08/2020") else { return }
+        
+        let geradorDePagamento = GeradorDePagamento(daoFalso, avaliador, pagamentos, dataAntiga)
+        geradorDePagamento.gera()
+        
+        let capturadorDeArgumento = ArgumentCaptor<Pagamento>()
+        verify(pagamentos).salva(capturadorDeArgumento.capture())
+        
+        let pagamentoGerado = capturadorDeArgumento.value
+        
+        let formatadorDeData = DateFormatter()
+        formatadorDeData.dateFormat = "ccc"
+        
+        guard let dataDoPagamento = pagamentoGerado?.getData() else { return }
+        let diaDaSemana = formatadorDeData.string(from: dataDoPagamento)
+        
+        XCTAssertEqual("Mon", diaDaSemana)
+           
+    }
+       
 
   
 
